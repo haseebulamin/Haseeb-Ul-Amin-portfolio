26d046c1772bca210c117873b6a074dde0ecec7b
haseebulamin <haseebulamin07@gmail.com>
2024-01-03 22:57:52 +0500

    satup home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './Home.scss'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div id="home">
+    <div className="row">
+      <div className="col-md-4"></div>
+      <div className="col-md-6"></div>
+    </div>
+    </div>
   )
 }
