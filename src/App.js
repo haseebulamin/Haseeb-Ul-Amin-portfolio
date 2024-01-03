ce042bfb0d541148793b6388af078dd8966dbc8a
haseebulamin <haseebulamin07@gmail.com>
2024-01-03 20:06:35 +0500

    set app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Mainframe from './Components/Mainframe/Mainframe';
-// import Navbar from './Components/Navbar/Navbar';
+// import Mainframe from './Components/Mainframe/Mainframe';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    // <Navbar/>
-    <Mainframe/>
+    <section id='app' className="h-screen">
+    <Navbar/>
+    <div className="h-full border-2 border-round-lg  m-3"> hlo</div>
+    </section>
   );
 }
 
--- src/Components/Mainframe/Mainframe.jsx
+++ src/Components/Mainframe/Mainframe.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import './Mainframe.scss'
 // import bgImage from '../../../Images/herophoto.png'
-import Navbar from '../Navbar/Navbar'
+// import Navbar from '../Navbar/Navbar'
 export default function Mainframe() {
   return (
-    <section id='mainframe' className="h-screen">
-    <Navbar/>
-    </section>
+    <div>main framae</div>
   )
 }
