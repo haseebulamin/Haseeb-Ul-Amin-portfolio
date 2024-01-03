1bc5470dd86b8f025b12ed748bbcc235d8406b1b
haseebulamin <haseebulamin07@gmail.com>
2024-01-03 19:24:56 +0500

    create navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,48 +1,22 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import './Navbar.scss'
-import { SpeedDial } from 'primereact/speeddial';
-import { Toast } from 'primereact/toast';
 export default function Navbar() {
-
-    const toast = useRef(null);
-    const items = [
-        {
-            label: 'Add',
-            icon: 'pi pi-pencil',
-            command: () => {
-                toast.current.show({ severity: 'info', summary: 'Add', detail: 'Data Added' });
-            }
-        },
-        {
-            label: 'Update',
-            icon: 'pi pi-refresh',
-            command: () => {
-                toast.current.show({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
-            }
-        },
-        {
-            label: 'Delete',
-            icon: 'pi pi-trash',
-            command: () => {
-                toast.current.show({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
-            }
-        },
-        {
-            label: 'React Website',
-            icon: 'pi pi-external-link',
-            command: () => {
-                window.location.href = 'https://facebook.github.io/react/';
-            }
-        }
-    ];
-
-    return (
-        <div className="card">
-            <div >
-                <Toast ref={toast} />
-                <SpeedDial className='love' model={items} direction="right" />
-            </div>
-        </div>
-    )
-  
+  return (
+ <nav className="flex justify-content-center py-1">
+      <ul className="flex justify-content-center list-none m-0 p-0  border-round-3xl border-primary">
+        <li className="nav-item py-2 px-7 font-normal text-2xl border-round-3xl">
+          <a className="nav-link " aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item py-2 px-7 font-normal text-2xl border-round-3xl">
+          <a className="nav-link  "  href="/">About</a>
+        </li>
+        <li className="nav-item py-2 px-7 font-normal text-2xl border-round-3xl">
+          <a className="nav-link"  href="/">Services</a>
+        </li>
+        <li className="nav-item py-2 px-7 font-normal text-2xl border-round-3xl">
+          <a className="nav-link "  href="/">Contact</a>
+        </li>
+      </ul>
+ </nav>
+  )
 }
