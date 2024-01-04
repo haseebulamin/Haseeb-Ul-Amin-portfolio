c35fe2177b3b67d5c019ba1bed9dc1b864b75e19
haseebulamin <haseebulamin07@gmail.com>
2024-01-04 21:10:01 +0500

    create home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './Home.scss'
 export default function Home() {
   return (
-    <div id="home">
-    <div className="row">
-      <div className="col-md-4">col 4</div>
-      <div className="col-md-6"> col 8</div>
+    <div id="home" className="h-full">
+    <div className="row h-full">
+      <div className="col-md-5 col-sm-12 bg-warning ">
+        
+      </div>
+
+      <div className="col-md-7 col-sm-12 bg-danger"> col 8</div>
     </div>
     </div>
   )
