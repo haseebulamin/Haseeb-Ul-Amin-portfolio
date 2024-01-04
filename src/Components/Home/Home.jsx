e42208a07cd53f38ec1c2ca26a2da360dbab708d
haseebulamin <haseebulamin07@gmail.com>
2024-01-04 23:51:07 +0500

    home content complate

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,15 +11,14 @@ export default function Home() {
             <span className="icon main-color"><i class="fa-solid fa-caret-down"></i></span>
           </div>
           <h1 className="text-6xl font-bold">I'm Haseeb Ul Amin</h1>
-          <h4 className="text-4xl font-medium main-color">I'm frontend developer.</h4>
+          <h4 className="text-4xl font-medium">I am frontend developer.</h4>
           <p>Experienced React.js Frontend Developer, transforming designs into sleek, user-friendly interfaces for optimal digital experiences.</p>
-          <button>Hire me</button>
-          <button className="py-2 px-4 border-color bg-transparent  font-normal text-lg border-round-3xl"><a href="/" className="no-underline main-color">Download CV</a></button>
+          <button className="py-2 px-3 mr-2 border-color bg-transparent outline-none font-normal text-md border-round-3xl"><a href="/" className="no-underline main-color">More About me</a></button>
+          <button className="py-2 px-3 mx-2 border-color bg-transparent outline-none font-normal text-md border-round-3xl"><a href="/" className="no-underline main-color" >Download CV</a></button>
           </div>
-            
         </div>
 
-        <div className="col-md-7 col-sm-12 bg-danger"> col 8</div>
+        <div className="col-md-7 col-sm-12 "> col 8</div>
       </div>
     </div>
   );
