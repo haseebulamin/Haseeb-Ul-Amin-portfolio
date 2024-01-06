547b8cc12f919d5867ad146ad75ea3437455c00d
haseebulamin <haseebulamin07@gmail.com>
2024-01-07 00:25:27 +0500

    all name

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './Resume.scss'
 export default function Resume() {
   return (
-    <div id="resume" className="h-full text-white">
-        <h1>Haseeb Ul Amin</h1>
-        <p>Frontend Developer</p>
+    <div id="resume" className="h-full text-white px-6 py-3">
+        <h1 className='font-bold'>Haseeb Ul Amin</h1>
+        <p className='font-bold'>Frontend Developer</p>
+        <p>Experienced Software Engineer with a demonstrated history of working in the computer software industry. Skilled in JavaScript, React.js, ES6, Git, React State, JSX, React Hooks, Frontend Developer. Strong engineering professional with a Bachelor of Science - BS focused in Computer Software Engineering from COMSTAS University Islamabad (Sahiwal Campus) of Pakistan.</p>
       <div className="row h-full">
+        <p>haseebulamin07</p>
       </div>
     </div>
   )
