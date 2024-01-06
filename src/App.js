fbb09af57c5b25b770a969c7a601668e6ae387c6
haseebulamin <haseebulamin07@gmail.com>
2024-01-07 00:20:54 +0500

    create resume

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 // import Contact from './Components/Contact/Contact'
 import About from './Components/About/About';
+import Resume from './Components/Resume/Resume';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
     <div className="frame border-2 border-round-3xl  mx-3 mb-3 mt-2 "> 
     {/* <Home/> */}
     {/* <Contact/> */}
-    <About/>
+    {/* <About/> */}
+    <Resume/>
     </div>
     </section>
   );
---
+++ src/Components/Resume/Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Resume.scss'
+export default function Resume() {
+  return (
+    <div id="resume" className="h-full text-white">
+        <h1>Haseeb Ul Amin</h1>
+        <p>Frontend Developer</p>
+      <div className="row h-full">
+      </div>
+    </div>
+  )
+}
