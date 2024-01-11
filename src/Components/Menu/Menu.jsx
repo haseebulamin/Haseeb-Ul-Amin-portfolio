4b40cc54d9715cc04ff9547e150ba3169b54a5b7
haseebulamin <haseebulamin07@gmail.com>
2024-01-11 14:21:47 +0500

    fix menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -12,14 +12,14 @@ export default function Menu() {
   return (
     <>
     <input type="checkbox" id='active' className='hidden'/>
-    <label htmlFor='active' className="menu-btn fixed  bg-danger shadow-8 text-center cursor-pointer z-2"><i class="fa-solid fa-equals fa-xl text-black"></i></label>
+    <label htmlFor='active' className="menu-btn fixed shadow-8 text-center cursor-pointer z-2"><i class="fa-solid fa-equals fa-xl text-black"></i></label>
     <div className="wrapper fixed bg-white top-0 h-full w-full">
-        <ul className="menu list-none flex flex-column text-center">
-            <li className='mb-3 '><h1 className='item inline-block  no-underline text-black font-bold  aisle'>Home</h1></li>
-            <li className='my-3 '><h1  className='item inline-block  no-underline text-black font-bold  about'>About</h1></li>
-            <li className='my-3 '><h1  className='item inline-block  no-underline text-black font-bold  work'>Resume</h1></li>
-            <li className='my-3 '><h1  className='item inline-block  no-underline text-black font-bold  career'>Portfolio</h1></li>
-            <li className='my-3 '><h1  className='item inline-block  no-underline text-black font-bold  contact'>Contact</h1></li>
+        <ul className="menu list-none h-full flex flex-column justify-content-center align-items-center">
+            <li className='mb-1 '><h1 className='item inline-block  no-underline text-white font-bold  aisle'>Home</h1></li>
+            <li className='my-1 '><h1  className='item inline-block  no-underline text-white font-bold  about'>About</h1></li>
+            <li className='my-1 '><h1  className='item inline-block  no-underline text-white font-bold  work'>Resume</h1></li>
+            <li className='my-1 '><h1  className='item inline-block  no-underline text-white font-bold  career'>Portfolio</h1></li>
+            <li className='my-1 '><h1  className='item inline-block  no-underline text-white font-bold  contact'>Contact</h1></li>
         </ul>
     </div>
     </>
