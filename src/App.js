6ed3bae5b79e5eaa9199bed410eaf72e156153f7
haseebulamin <haseebulamin07@gmail.com>
2024-01-13 19:32:25 +0500

    set home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
-// import Home from './Components/Home/Home';
+import Home from './Components/Home/Home';
 // import Navbar from './Components/Navbar/Navbar';
-import Contact from './Components/Contact/Contact'
+// import Contact from './Components/Contact/Contact'
 // import About from './Components/About/About';
 // import Resume from './Components/Resume/Resume';
-import Menu from './Components/Menu/Menu';
+// import Menu from './Components/Menu/Menu';
 
 function App() {
   return (
     <>
-    {/* <div className="frame border-2 border-round-3xl  mx-3 mb-3 mt-2 "></div> */}
-    <section id='app' className="h-screen p-6 ">
-    <div className="frame border-2 border-round-3xl h-full relative ">
-    <Menu/>
-    {/* <Home/> */}
-    <Contact/>
-    </div>
-     </section>
+    <Home/>
     </>
   );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Home.scss";
 import "../Common/Button.scss"
+import profile from "../../Images/profile.jpg"
+// import Navbar from '../Navbar/Navbar'
 export default function Home() {
   return (
-    <div id="home" className="h-full text-white">
-      <div className="row h-full">
-        <div className="col-md-5 col-sm-12 flex flex-column justify-content-center ">
+    <div id="home" className="text-white ">
+      <div className="container ">
+      <div className="row h-screen">
+        <div className="col-md-6 col-sm-12 flex flex-column justify-content-center ">
           <div className="content pl-6">
           <div className="msg_box my-2">
             <span className="box main-bg text-white">Hello</span>
@@ -16,11 +19,14 @@ export default function Home() {
           <h4 className="text text-4xl font-medium">Frontend Developer</h4>
           </div>
           <p className="mt-2 ">Experienced front-end Developer with a demonstrated history of working in the computer software industry.</p>
-          <div className="button inline-block mr-2 mt-2" ><a type="button" href="/" className="no-underline main-color px-4 py-2 font-normal text-md border-round-3xl bg-transparent overflow-hidden relative ">More About me</a></div>
-          <div className="button inline-block ml-2 mt-2" ><a type="button" href="/CV.pdf" download="Mycv.pdf"  className="no-underline main-color px-4 py-2 font-normal text-md border-round-3xl bg-transparent overflow-hidden relative ">Download CV</a></div>
+          <div className="button inline-block mr-2 mt-2" ><a type="button" href="/" className="no-underline main-color px-4 py-2 bg-low font-normal text-md border-round-3xl bg-transparent overflow-hidden relative ">More About me</a></div>
+          <div className="button inline-block ml-2 mt-2" ><a type="button" href="/CV.pdf" download="Mycv.pdf"  className="no-underline bg-low main-color px-4 py-2 font-normal text-md border-round-3xl bg-transparent overflow-hidden relative ">Download CV</a></div>
           </div>
         </div>
-        <div className="col-md-7 col-sm-12 "> col 8</div>
+        <div className="col-md-6 col-sm-12 flex align-items-center justify-content-center"> 
+        <img src={profile} alt="not insert" height={300} width={300} className="profile rounded-circle"/>
+        </div>
+      </div>
       </div>
     </div>
   );
