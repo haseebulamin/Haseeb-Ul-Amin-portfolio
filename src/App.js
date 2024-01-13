9ce08729af340339fe37965706cbefa08219ef1e
haseebulamin <haseebulamin07@gmail.com>
2024-01-13 20:09:39 +0500

    set about

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Home from './Components/Home/Home';
 // import Navbar from './Components/Navbar/Navbar';
 // import Contact from './Components/Contact/Contact'
-// import About from './Components/About/About';
+import About from './Components/About/About';
 // import Resume from './Components/Resume/Resume';
 // import Menu from './Components/Menu/Menu';
 
 function App() {
   return (
     <>
-    <Home/>
+    {/* <Home/> */}
+    <About/>
     </>
   );
 }
