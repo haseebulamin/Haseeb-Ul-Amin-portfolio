9c1335158305208d33cc9b1028dea1f38de2572b
haseebulamin <haseebulamin07@gmail.com>
2024-01-19 23:02:18 +0500

    col

--- src/Components/Quality/Quality.jsx
+++ src/Components/Quality/Quality.jsx
@@ -6,8 +6,18 @@ export default function Quality() {
     <div id='quality' className='bg-dark text-white'>
         <div className="row h-full">
         <h1 className='text-6xl my-5 main-color text-center font-bold'>Quality</h1>
-        <div className="col-md-6 flex justify-content-center align-items-center p-5">education</div>
-        <div className="col-md-6 flex justify-content-center align-items-center p-5">experiance</div>
+        <div className="col-md-6 flex justify-content-center align-items-center p-5">education
+        <div className="4"></div>
+        <div className="4"></div>
+        <div className="4"></div>
+        <div className="4"></div>
+        </div>
+        <div className="col-md-6 flex justify-content-center align-items-center p-5">experiance
+        <div className="4"></div>
+        <div className="4"></div>
+        <div className="4"></div>
+        <div className="4"></div>
+        </div>
         </div>
     </div>
   )
