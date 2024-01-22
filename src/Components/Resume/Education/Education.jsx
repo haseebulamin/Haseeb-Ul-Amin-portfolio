4f5fb1dc54613bed63b1cdd5b745cf2e11a5450c
haseebulamin <haseebulamin07@gmail.com>
2024-01-22 17:53:07 +0500

    educate

--- src/Components/Resume/Education/Education.jsx
+++ src/Components/Resume/Education/Education.jsx
@@ -2,29 +2,25 @@ import React from 'react'
 import './Education.scss'
 import '../../Common/Variables.scss'
 export default function Education() {
-  return (
-    <div id='education'>
-        <div className="col-8 bg-dark text-white">
-            <div className="row">
-                <div className="col-md-6">
-                    <p className='text-xl main-color'>Education</p>
-                    <h1 className='font-semibold text-6xl'>Learning experiance in a <span className='main-color'>Various institutions</span></h1>
-                </div>
-                <div className="col-md-6 flex justify-content-center ">
-                   <div className='educate absolute'>
-                        <h1 className='text-2xl font-bold'><i class="fa-solid fa-circle fa-2xs me-3"></i>Graduation</h1>
-                        <p className='text-sm ml-5 mb-0'>BSCS(2022 - Present)</p>
-                        <p className='text-lg font-bold ml-5'>COMSATS University, Sahiwal Campus</p>
-                        <h1 className='text-2xl font-bold'><i class="fa-solid fa-circle fa-2xs me-3"></i>Intermediate</h1>
-                        <p className='text-sm ml-5 mb-0'>ICS(2020 - 2022)</p>
-                        <p className='text-lg font-bold ml-5'>Informatics Colleges, Kamalia Campus</p>
-                        <h1 className='text-2xl font-bold'><i class="fa-solid fa-circle fa-2xs me-3"></i>Matriculation</h1>
-                        <p className='text-sm ml-5 mb-0'>Science(2018 - 2020)</p>
-                        <p className='text-lg font-bold ml-5'>Govt High School, 739 jakhar</p>
-                   </div>
-                </div>
-            </div>
-        </div>
+return (
+<div className="row text-white bg-dark g-0 ">
+  <div className="col-md-6 ">
+    <p className='text-xl main-color'>Education</p>
+    <h1 className='font-semibold text-6xl'>Learning experiance in a <span className='main-color'>Various institutions</span></h1>
+  </div>
+  <div className="col-md-6 flex justify-content-center bg-danger ">
+    <div className='educate absolute'>
+      <h1 className='text-2xl font-bold'><i class="fa-solid fa-circle fa-2xs me-3"></i>Graduation</h1>
+      <p className='text-sm ml-5 mb-0'>BSCS(2022 - Present)</p>
+      <p className='text-lg font-bold ml-5'>COMSATS University, Sahiwal Campus</p>
+      <h1 className='text-2xl font-bold'><i class="fa-solid fa-circle fa-2xs me-3"></i>Intermediate</h1>
+      <p className='text-sm ml-5 mb-0'>ICS(2020 - 2022)</p>
+      <p className='text-lg font-bold ml-5'>Informatics Colleges, Kamalia Campus</p>
+      <h1 className='text-2xl font-bold'><i class="fa-solid fa-circle fa-2xs me-3"></i>Matriculation</h1>
+      <p className='text-sm ml-5 mb-0'>Science(2018 - 2020)</p>
+      <p className='text-lg font-bold ml-5'>Govt High School, 739 jakhar</p>
     </div>
+  </div>
+</div>
   )
 }
--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Resume.scss'
 import clientDp from '../../Images/profile.jpg'
+import Education from './Education/Education'
 export default function Resume() {
   return (
     <div id="carouselExampleCaptions" className="carousel slide overflow-hidden" data-bs-ride="false" >
-      <h1>My Resume</h1>
     <div className="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1" ></button>
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"  aria-label="Slide 2" ></button>
@@ -21,15 +21,11 @@ export default function Resume() {
       </div>
 
       <div className=" col-10  ">
-        <div className="carousel-inner">
-        <div className="carousel-item active ">
-        <div className="h-screen flex flex-column align-items-center justify-content-center">
-        <h1 className="text-7xl my-5 font-bold">Our Client Say</h1>
-        <img src={clientDp} className="clienttImg border-circle my-5" height={250} width={250} alt="" />
-        <p className="text-xl text-center">
-        Tom and Jerry were created without voices primarily for comedic effect. The creators of the cartoon series, William Hanna and Joseph Barbera, believed that the characters' actions and expressions would be more effective in telling a story than their voices.
-        </p>
-        <h5 className="text-3xl font-bold">Tom Morris</h5>
+        <div className="carousel-inner h-screen">
+        <h1 className='text-6xl main-color my-3 font-bold text-center'>My Resume</h1>
+        <div className="carousel-item active h-full">
+        <div className="h-full flex flex-column align-items-center justify-content-center">
+        <Education/>
         </div>
         </div>
         <div className="carousel-item">
