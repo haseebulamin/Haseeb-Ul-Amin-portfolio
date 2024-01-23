537ceea63c0caf65ef774b51a95497b52e4009ba
haseebulamin <haseebulamin07@gmail.com>
2024-01-23 18:45:19 +0500

    ex

--- src/Components/Resume/Experience/Experience.jsx
+++ src/Components/Resume/Experience/Experience.jsx
@@ -3,12 +3,12 @@ import './Experience.scss'
 import '../../Common/Variables.scss'
 export default function Experience() {
   return (
-   <div className="bg-dark h-full">
+   <div className="bg-dark">
      <div className="row text-white  g-0 ">
   <div className="col-md-7  p-3">
   <div class="card mb-3 w-full bg-low text-white border-0">
   <div class="row g-0 p-2 ">
-    <div class="col-2 flex justify-content-center align-items-center border-right-1">
+    <div class="col-2 flex justify-content-center align-items-center border-right-1 border-color">
       <span className='main-color text-5xl font-bold p-2'>01</span>
     </div>
     <div class="col-10 p-0 ">
@@ -21,7 +21,7 @@ export default function Experience() {
 </div>
   <div class="card mb-3 w-full bg-low text-white border-0">
   <div class="row g-0 p-2 ">
-    <div class="col-2 flex justify-content-center align-items-center border-right-1">
+    <div class="col-2 flex justify-content-center align-items-center border-right-1 border-color">
       <span className='main-color text-5xl font-bold p-2'>02</span>
     </div>
     <div class="col-10 p-0">
@@ -34,12 +34,12 @@ export default function Experience() {
 </div>
   <div class="card mb-3 w-full bg-low text-white border-0">
   <div class="row g-0 p-2 ">
-    <div class="col-2 flex justify-content-center align-items-center border-right-1">
+    <div class="col-2 flex justify-content-center align-items-center border-right-1 border-color">
       <span className='main-color text-5xl font-bold p-2'>03</span>
     </div>
     <div class="col-10 p-0">
       <div class="card-body py-0 pr-0 pl-3 mt-2">
-      <div className='text-2xl font-bold'>TechDev - <span className='font-medium'>UI Design</span></div>
+      <div className='text-2xl font-bold'>TechDev - <span className='font-medium'>UI Designer</span></div>
       <span className='text-sm  mb-0 main-color ms-auto'>Mar 2022 - Aug 2022</span>
       </div>
     </div>
