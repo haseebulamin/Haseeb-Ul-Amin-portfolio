3c0c0f222a1c4abd89e6307aa86bcf6e0047220b
haseebulamin <haseebulamin07@gmail.com>
2024-01-25 20:19:11 +0500

    news

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -61,7 +61,7 @@ export default function Portfolio() {
             </div>
           </div>
         </div>
-        {/* .......5  */}
+        {/* .......4   */}
         <div className="col-md-4">
           <div className="card w-100 shadow animate__animated animate__fadeInUp mt-5">
             <img src={gym} className="card-img-top" alt="..." />
@@ -71,6 +71,18 @@ export default function Portfolio() {
                 <a href='https://github.com/haseebulamin/Portfolio' className='text-white'><i className="text-2xl fa-brands fa-github"></i></a>
               </div>
               <p className="card-text">From using HtML & CSS</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card w-100 shadow animate__animated animate__fadeInUp mt-5">
+            <img src={newshour} className="card-img-top" alt="..." />
+            <div className="card-body bg-low">
+              <div className="flex justify-content-between ">
+                <span className="font-bold text-secondary ">News App</span>
+                <a href='https://github.com/haseebulamin/Portfolio' className='text-white'><i className="text-2xl fa-brands fa-github"></i></a>
+              </div>
+              <p className="card-text">Single page app using React JS</p>
             </div>
           </div>
         </div>
