277064aa1f028bb250f9badf92c5ae3638289a7f
haseebulamin <haseebulamin07@gmail.com>
2024-01-25 20:12:59 +0500

    retaurant

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -11,7 +11,7 @@ import htmlport from '../../Images/html portfolio.png';
 import lifebuild from '../../Images/life build.png';
 export default function Portfolio() {
   return (
-    <section id="portfolio" className="pt-2 pb-5 bg-dark">
+    <section id="portfolio" className="pt-2 pb-5 bg-black">
     <div className="container ">
       {/* row 1 */}
       <div className="row justify-content-center">
@@ -22,6 +22,7 @@ export default function Portfolio() {
       </div>
       {/* row 2 */}
       <div className="row justify-content-center">
+        {/* ......... 1 */}
         <div className="col-md-4">
           <div className="card w-100 shadow animate__animated animate__fadeInUp mt-5">
             <img src={protreact} className="card-img-top" alt="..." />
@@ -34,12 +35,26 @@ export default function Portfolio() {
             </div>
           </div>
         </div>
+        {/* ..........2 */}
         <div className="col-md-4">
           <div className="card w-100 shadow animate__animated animate__fadeInUp mt-5">
             <img src={lifebuild} className="card-img-top" alt="..." />
             <div className="card-body bg-low">
               <div className="flex justify-content-between ">
                 <span className="font-bold text-secondary ">Life build</span>
+                <a href='https://github.com/haseebulamin/Portfolio' className='text-white'><i className="text-2xl fa-brands fa-github"></i></a>
+              </div>
+              <p className="card-text">Landing page using Bootstrap</p>
+            </div>
+          </div>
+        </div>
+        {/* .........3 */}
+        <div className="col-md-4">
+          <div className="card w-100 shadow animate__animated animate__fadeInUp mt-5">
+            <img src={returent} className="card-img-top" alt="..." />
+            <div className="card-body bg-low">
+              <div className="flex justify-content-between ">
+                <span className="font-bold text-secondary ">Restaurant Page</span>
                 <a href='https://github.com/haseebulamin/Portfolio' className='text-white'><i className="text-2xl fa-brands fa-github"></i></a>
               </div>
               <p className="card-text">Landing page using Bootstrap</p>
