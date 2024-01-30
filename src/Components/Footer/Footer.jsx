563dea17e3598df4e9a1fd484ebd98ed065bd39a
haseebulamin <haseebulamin07@gmail.com>
2024-01-31 00:34:41 +0500

    add link in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './Footer.scss'
 import '../Common/Variables.scss'
 import hlogo from '../../Images/Hlogo.png'
+import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
       <>
     <div id="footer" className='text-white'>
         <hr />
         <div className='py-5 text-center' >
-          <a href="/home"> <img src={hlogo} alt="not" height={70} /></a>
-           <p className='font-medium'>Copyright  2023 All rights reserved | <a href="/" className='no-underline main-color'> Haseeb Ul Amin</a></p>
+          <Link to="/"> <img src={hlogo} alt="not" height={70} /></Link>
+           <p className='font-medium'>Copyright <i class="fa-regular fa-copyright"></i> 2023 All rights reserved | <a href="https://www.linkedin.com/in/haseebulamin/" className='no-underline main-color'> Haseeb Ul Amin</a></p>
 
         </div>
 
--- src/Components/HireMe/HireMe.jsx
+++ src/Components/HireMe/HireMe.jsx
@@ -11,7 +11,7 @@ export default function HireMe() {
          <div className="w-9 text-center my-3">
          <p className=' main-color font-semibold mb-0 py-2'>Do You Have Any Project ?</p>
          <p className='text-6xl text-white font-bold'>Available for Web development projects, utilizing expert programming tools and skills.</p>
-         <div className="button inline-block" ><Link type="button" to="/contact" className="no-underline bg-low main-color px-4 py-2 font-normal text-md border-round-3xl bg-transparent overflow-hidden relative ">Hire Me</Link></div>
+         <div className="button inline-block" ><Link type="button"  to="/contact" className="no-underline bg-low main-color px-4 py-2 font-normal text-md border-round-3xl bg-transparent overflow-hidden relative " >Hire Me</Link></div>
          </div>
         </div>
     </div>
