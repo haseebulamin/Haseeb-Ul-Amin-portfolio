ac99175744469db84a09d2080dbd8b82da1877e4
haseebulamin <haseebulamin07@gmail.com>
2024-01-30 20:13:17 +0500

    create menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Portfolio from './Components/Portfolio/Portfolio';
+// import Portfolio from './Components/Portfolio/Portfolio';
 // import MyCertificates from './Components/MyCertificates/MyCertificates';
 // import Footer from './Components/Footer/Footer';
-// import Navbar from './Components/Navbar/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 // import Resume from './Components/Resume/Resume';
 // import Home from './Components/Home/Home';
 // import Contact from './Components/Contact/Contact';
@@ -13,7 +13,7 @@ function App() {
   return (
     <div id='app'  >
     <div className='h-full appb'>
-      {/* <Navbar/> */}
+      <Navbar/>
       {/* <Resume/> */}
      {/* <Loader/> */}
     {/* <Home/> */}
@@ -21,7 +21,7 @@ function App() {
     {/* <Education/> */}
     {/* <Experience/> */}
     {/* <MyCertificates/> */}
-    <Portfolio/>
+    {/* <Portfolio/> */}
     {/* <Contact/> */}
     {/* // <Footer/> */}
     </div>
---
+++ src/Components/Navbar/Menu/Menu.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Menu.scss'
+export default function Menu() {
+  return (
+    <div>Menu</div>
+  )
+}
