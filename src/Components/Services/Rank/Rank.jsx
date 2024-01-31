00a60bc1a267a1fae4f5b66eab00877b32dfb3f8
haseebulamin <haseebulamin07@gmail.com>
2024-01-31 23:14:15 +0500

    create review

--- src/Components/Services/Rank/Rank.jsx
+++ src/Components/Services/Rank/Rank.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Rank.scss'
 import '../../Common/Variables.scss'
 export default function Rank() {
   return (
@@ -7,9 +6,9 @@ export default function Rank() {
         <div className="container bg-low border-round-2xl p-6">
             <div className="row animate__animated animate__fadeInUp">
                 <div className="col-sm-6  col-md-4 flex justify-content-center">
-                    <div className="wrap text-white d-flex py-2">
+                    <div className="wrap text-white d-flex my-3">
                         <div className="icon">
-                        <i class="fa-solid fa-id-card text-6xl mt-4"></i>
+                        <i class="fa-solid fa-id-card text-6xl mt-3"></i>
                         </div>
                         <div className="text ml-3">
                             <h1 className='font-bold'>15+</h1>
@@ -19,28 +18,26 @@ export default function Rank() {
 
                 </div>
                 <div className="col-sm-6  col-md-4 flex justify-content-center">
-                    <div className="wrap text-white d-flex py-2">
+                    <div className="wrap text-white d-flex my-3">
                         <div className="icon">
-                        <i class="fa-solid fa-diagram-project text-6xl mt-4"></i>
+                        <i class="fa-solid fa-diagram-project text-6xl mt-3"></i>
                         </div>
                         <div className="text ml-3">
                             <h1 className='font-bold'>10+</h1>
                             <span className='font-bold uppercase'>PROJECT COMPLETED</span>
                         </div>
                     </div>
-
                 </div>
                 <div className="col-sm-6  col-md-4 flex justify-content-center ">
-                    <div className="wrap text-white d-flex py-2">
+                    <div className="wrap text-white d-flex my-3">
                         <div className="icon">
-                        <i class="fa-solid fa-star text-6xl mt-4"></i>
+                        <i class="fa-solid fa-star text-6xl mt-3"></i>
                         </div>
                         <div className="text ml-3">
                             <h1 className='font-bold'>40+</h1>
                             <span className='font-bold uppercase'>POSITIVE REVIEWS</span>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
---
+++ src/Components/Services/Review/Review.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Review.scss'
+export default function review() {
+  return (
+    <div>review</div>
+  )
+}
