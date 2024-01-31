489075befde508975ad7393e75341addf940bb82
haseebulamin <haseebulamin07@gmail.com>
2024-01-31 21:21:00 +0500

    create services

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
-import Menu from './Components/Navbar/Menu/Menu';
+import Services from './Components/Services/Services';
 // import Menu from './Components/Navbar/Menu/Menu';
-import Portfolio from './Components/Portfolio/Portfolio';
-import MyCertificates from './Components/MyCertificates/MyCertificates';
+// import Portfolio from './Components/Portfolio/Portfolio';
+// import MyCertificates from './Components/MyCertificates/MyCertificates';
 // import Footer from './Components/Footer/Footer';
 // import Navbar from './Components/Navbar/Navbar';
-import Resume from './Components/Resume/Resume';
-import Home from './Components/Home/Home';
-import Contact from './Components/Contact/Contact';
+// import Resume from './Components/Resume/Resume';
+// import Home from './Components/Home/Home';
+// import Contact from './Components/Contact/Contact';
 // import Loader from './Components/Loader/Loader';
 // import About from './Components/About/About';
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 function App() {
   return (
     <>
-      <Router>
+    <Services/>
+      {/* <Router>
       <Routes>
       <Route exact  path='/menu' element={<Menu key="/menu"/>} />
       <Route exact  path='/' element={<Home key="/home"/>} />
@@ -23,7 +24,7 @@ function App() {
       <Route exact  path='/portfolio' element={<Portfolio key="/portfolio"/>} />
       <Route exact  path='/contact' element={<Contact key="/contact"/>} />
       </Routes>
-    </Router>
+    </Router> */}
     </>
   );
 }
