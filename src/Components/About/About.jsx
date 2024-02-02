e2d32edd0945656b06d944c0ad8c4010cec1e5af
haseebulamin <haseebulamin07@gmail.com>
2024-02-02 22:40:16 +0500

    tilt image

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import './About.scss'
 import aboutpic from '../../Images/about.jpg'
+import './About'
 import { Link } from 'react-router-dom'
 export default function About() {
+
+  const tiltEls = document.querySelectorAll('.tilt')
+
+const tiltMove = (x, y) => `perspective(500px) scale(1.1) rotateX(${x}deg) rotateY(${y}deg)`
+
+tiltEls.forEach(tilt => {
+    const height = tilt.clientHeight
+    const width = tilt.clientWidth
+
+    tilt.addEventListener('mousemove', (e) => {
+        const x = e.layerX
+        const y = e.layerY
+        const multiplier = 40
+
+        const xRotate = multiplier * ((x - width / 2) / width)
+        const yRotate = -multiplier * ((y - height / 2) / height)
+
+        tilt.style.transform = tiltMove(xRotate, yRotate)
+    })
+
+    tilt.addEventListener('mouseout', () => tilt.style.transform = tiltMove(0, 0))
+})
+
   return (
     <div id="about" className="h-full overflow-hidden text-white">
     <div className="row h-full g-0">
     <div className='text-center py-4'><p className='about-title text-xl inline-block font-bold uppercase main-color pl-6'>About me</p></div>
-      <div className="col-lg-5  flex align-items-center justify-content-center py-5">
-       <img src={aboutpic} alt="Description of the about section" className='shadow-8 border-round-3xl' />
+      <div className="col-lg-5  flex align-items-center justify-content-center py-6">
+       <img src={aboutpic} alt="Description of the about section" className='tilt shadow-8 border-round-3xl'/>
       </div>
       <div className="col-lg-7  flex flex-column justify-content-center px-7 py-5">
         <p>I'm Haseeb ul Amin, a Computer Science student at COMSATS University, Sahiwal Campus, Pakistan. Proficient in C, C++, HTML, CSS, JavaScript, and Java, I recently completed a Frontend Developer Course from Great Learning.</p>
