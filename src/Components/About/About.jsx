88b270de20e6ebb8967bdf557ba1188e26c69547
haseebulamin <haseebulamin07@gmail.com>
2024-07-29 16:00:23 +0500

    animation link add

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -34,16 +34,16 @@ export default function About() {
   return (
     <div id="about" className="h-full overflow-hidden text-white pt-6">
     <div className="row h-full g-0">
-    <div className='text-center py-4'><p className='title-line text-xl inline-block font-bold uppercase main-color pl-6'>About me</p></div>
+    <div className='text-center pt-4 ' ><p data-aos="zoom-in" className='title-line text-xl inline-block font-bold uppercase main-color pl-6'>About me</p></div>
       <div className="col-lg-5  flex align-items-center justify-content-center py-6">
-       <img src={aboutpic} alt="Description of the about section" className='tilt shadow-8 border-round-3xl'/>
+       <img data-aos="fade-right" data-aos-offset="2000" src={aboutpic} alt="Description of the about section" className='tilt shadow-8 border-round-3xl'/>
       </div>
       <div className="col-lg-7  flex flex-column justify-content-center px-7 py-5">
-        <p>I'm Haseeb ul Amin, a Computer Science student at COMSATS University, Sahiwal Campus, Pakistan. Proficient in C, C++, HTML, CSS, JavaScript, and Java, I recently completed a Frontend Developer Course from Great Learning.</p>
-        <p>Actively applying my skills, I've initiated minor projects to further enhance my capabilities. Beyond academics, I thrive in extracurriculars, participating in hackathons, coding competitions, and bootcamps, fostering collaboration and innovative problem-solving.</p>
-        <p className='main-color font-bold uppercase text-lg'>My Vision</p>
-        <p>I'm driven to blend theoretical insights with hands-on innovation. My goal is to craft visually stunning interfaces that redefine user experiences. I aspire to architect dynamic applications, pushing the boundaries of interactivity and contributing to the forefront of web development.</p>
-        <div className="button inline-block mr-2 mt-2" ><Link type="button" to="/resume" className="bt no-underline main-color px-4 py-2 bg-low font-normal text-md border-round-3xl bg-transparent overflow-hidden relative ">Resume</Link></div>
+        <p data-aos="fade-down">I'm Haseeb ul Amin, a Computer Science student at COMSATS University, Sahiwal Campus, Pakistan. Proficient in C, C++, HTML, CSS, JavaScript, and Java, I recently completed a Frontend Developer Course from Great Learning.</p>
+        <p data-aos="fade-down">Actively applying my skills, I've initiated minor projects to further enhance my capabilities. Beyond academics, I thrive in extracurriculars, participating in hackathons, coding competitions, and bootcamps, fostering collaboration and innovative problem-solving.</p>
+        <p data-aos="fade-down" className='main-color font-bold uppercase text-lg'>My Vision</p>
+        <p data-aos="fade-down">I'm driven to blend theoretical insights with hands-on innovation. My goal is to craft visually stunning interfaces that redefine user experiences. I aspire to architect dynamic applications, pushing the boundaries of interactivity and contributing to the forefront of web development.</p>
+        <div className="button inline-block mr-2 mt-2" ><Link data-aos="fade-down" type="button" to="/resume" className="bt no-underline main-color px-4 py-2 bg-low font-normal text-md border-round-3xl bg-transparent overflow-hidden relative ">Resume</Link></div>
       </div>
     </div>
   </div>
